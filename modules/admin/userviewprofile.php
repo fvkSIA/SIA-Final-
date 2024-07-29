15fe51c88948778ef9c532427c5879fd9a6eea5b
Jemerito <101377289+Jemerito@users.noreply.github.com>
2024-07-30 01:58:34 +0800

    footer

--- modules/admin/userviewprofile.php
+++ modules/admin/userviewprofile.php
@@ -66,9 +66,6 @@ $conn->close();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap');
-
-        
 
 .form-control:focus {
     box-shadow: none;
